2ccd1207bfdf42f412ba9a86b7a4ed0c1ab71a57
SoutaTanaka <tanakakesouta@yahoo.co.jp>
2018-06-29 20:14:24 +0900

    [add] img.py fix

--- image/image.py
+++ image/image.py
@@ -1,12 +1,12 @@
 import numpy as np
 from PIL import Image, ImageFilter
 #import Image
-a = np.array([[[0, 0, 0],[255, 255, 255]],[[255,255,255],[0,0,0]]])
-img = np.tile(a,(5,5))
+a = np.array([[0,255],[255,0]])
+img = np.tile(a,(5000,5000))
 
 print(type(img))
-print(img)
-#jpimg = Image.fromarray(img)
+
+pimg = Image.fromarray(img)
 
 #pimg.show()
 
